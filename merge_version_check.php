eeabb44f3dc3641aa3941399146e17e2b7d86019
Nathan Malcolm <nmalcolm@hush.ai>
2014-01-30 02:16:39 +0000

    Add merge_version_check.php
    merge_version_check.php checks if a user is using an older version of the Merge System.

---
+++ merge_version_check.php
@@ -0,0 +1,14 @@
+---
+layout: blank
+permalink: merge_version_check.php
+---
+
+<?xml version="1.0" encoding="utf-8"?>
+<mybb_merge>
+	<friendly_name>{{ site.data.latest_merge.version }}</friendly_name>
+	<latest_version>{{ site.data.latest_merge.version }}</latest_version>
+	<version_code>{{ site.data.latest_merge.version_code }}</version_code>
+	<download_url>{{ site.data.latest_merge.url }}</download_url>
+	<release_date>{{ site.data.latest_merge.release }}</release_date>
+	<download_size>{{ site.data.latest_merge.size }}</download_size>
+</mybb_merge>
